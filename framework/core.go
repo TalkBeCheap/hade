69424e435ff2b6fad86756d08e1ba505078d018a
TalkBeCheap <xfy150150@gmail.com>
2022-01-03 12:34:59 +0800

    第三课路由router	router map[string]map[string]ControllerHandler // 二级map封装

--- framework/core.go
+++ framework/core.go
@@ -1,29 +1,82 @@
 package framework
 
 import (
-	"log"
 	"net/http"
+	"strings"
 )
 
 type Core struct {
-	router map[string]ControllerHandler
+	router map[string]map[string]ControllerHandler // 二级map
 }
 
 func NewCore() *Core {
-	return &Core{router: map[string]ControllerHandler{}}
+	getRouter := map[string]ControllerHandler{}
+	postRouter := map[string]ControllerHandler{}
+	putRouter := map[string]ControllerHandler{}
+	deleteRouter := map[string]ControllerHandler{}
+
+	router := map[string]map[string]ControllerHandler{}
+	router["GET"] = getRouter
+	router["POST"] = postRouter
+	router["DELETE"] = deleteRouter
+	router["PUT"] = putRouter
+	return &Core{router: router}
 }
+
+// ###################################
+//          http method wrap
+// ###################################
+
+// Get 匹配Get方法的路由规则
 func (c *Core) Get(url string, handler ControllerHandler) {
-	c.router[url] = handler
+	upperUrl := strings.ToUpper(url)
+	c.router["GET"][upperUrl] = handler
 }
 
-func (c *Core) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println("core.ServeHTTP")
-	ctx := NewContext(r, w)
+// POST 匹配Get方法的路由规则
+func (c *Core) Post(url string, handler ControllerHandler) {
+	upperUrl := strings.ToUpper(url)
+	c.router["POST"][upperUrl] = handler
+}
 
-	router := c.router["foo"]
+// Put 匹配Get方法的路由规则
+func (c *Core) Put(url string, handler ControllerHandler) {
+	upperUrl := strings.ToUpper(url)
+	c.router["PUT"][upperUrl] = handler
+}
+
+// Delete 匹配Get方法的路由规则
+func (c *Core) Delete(url string, handler ControllerHandler) {
+	upperUrl := strings.ToUpper(url)
+	c.router["DELETE"][upperUrl] = handler
+}
+
+func (c *Core) FindRouteByRequest(request *http.Request) ControllerHandler {
+	uri := request.URL.Path
+	method := request.Method
+	upperMethod := strings.ToUpper(method)
+	upperUri := strings.ToUpper(uri)
+	if methodHandlers, ok := c.router[upperMethod]; ok {
+		if handler, ok := methodHandlers[upperUri]; ok {
+			return handler
+		}
+	}
+	return nil
+}
+
+func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	// 封装自定义Context
+	ctx := NewContext(request, response)
+
+	// 寻找路由
+	router := c.FindRouteByRequest(request)
 	if router == nil {
+		ctx.Json(404, "not found")
 		return
 	}
-	log.Println("core.router")
-	router(ctx)
+	// 调用路由函数如果返回err 代表存在内部错误，返回500错误
+	if err := router(ctx); err != nil {
+		ctx.Json(500, "inner error")
+		return
+	}
 }
